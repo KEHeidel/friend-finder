019eabb12ebbe473b95186abda3b6b6a4be5ebfd
Kortnie Heidel <kortnie.evans@gmail.com>
2020-04-30 13:44:49 -0400

    sets up htmlroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,15 @@
 // ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+// ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
 module.exports = function(app) {
-    
+
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,10 @@
 // ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+// ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
@@ -6,4 +12,17 @@
 
 module.exports = function(app) {
     
-}
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+      });
+    
+      app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+      });
+
+        // If no matching route is found default to home
+        app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+        });
+
+};
