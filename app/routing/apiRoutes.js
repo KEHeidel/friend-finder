d9874985d6df34984d8825ea2e0f153a53c5a165
Kortnie Heidel <kortnie.evans@gmail.com>
2020-05-04 14:47:02 -0400

    adds comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,41 +1,41 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
-var path = require("path");
-
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
+// dependencies
 var friendsData = require("../data/friends");
 
 module.exports = function(app) {
 
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+  // api get requests
+  // below code handles when users "visit" a page.
 
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
   app.post("/api/friends", function(req, res) {
-      console.log(req.body.scores)
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (friendsData.length < 5) {
-      friendData.push(req.body);
-      res.json(true);
-    }
-    else {
-      res.json(false);
-    }
-  });
+      console.log(req.body.scores);
 
-}
+      var user = req.body;
+
+      for(var i = 0; i < user.scores.length; i++) {
+        user.scores[i] = parseInt(user.scores[i]);
+      }
+
+      var friendIndex = 0;
+      var minDifference = 40;
+
+      for (var i = 0; i < friendsData.length; i++) {
+        var totDifference = 0;
+        for (var j = 0; j < friendsData[i].scores.length; j++) {
+          var difference = Math.abs(user.scores[j] - friendsData[i].scores[j]);
+          totDifference += difference;
+        }
+        if(totDifference < minDifference) {
+          friendIndex = i;
+          minDifference = totDifference
+        }
+      }
+
+      friendsData.push(user);
+
+      res.json(friendsData[friendIndex]);
+  });
+};
